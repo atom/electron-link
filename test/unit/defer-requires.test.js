ff638335c2de43e0f278e765554423e39a7ada9d
Antonio Scandurra <as-cii@github.com>
2016-11-09 18:44:57 +0100

    :art:
    Signed-off-by: Nathan Sobo <nathan@github.com>

--- test/unit/defer-requires.test.js
+++ test/unit/defer-requires.test.js
@@ -2,93 +2,20 @@
 
 const assert = require('assert')
 const dedent = require('dedent')
-const deferRequires = require('../../lib/defer-requires')
+const processRequires = require('../../lib/process-requires')
 
-suite('deferRequires(source, deferredModules)', () => {
+suite('processRequires({source, didFindRequire})', () => {
   test('simple require', () => {
-    assert.equal(deferRequires(dedent`
+    const source = dedent`
       const a = require('a')
       const b = require('b')
       function main () {
         return a + b
       }
-    `, new Set(['a'])), dedent`
-      let a;
-
-      function get_a() {
-        return a = a || require('a');
-      }
-
-      const b = require('b')
-      function main () {
-        return get_a() + b
-      }
-    `)
-  })
-
-  test('top-level variables assignments that depend on previous requires', () => {
-    assert.equal(deferRequires(dedent`
-      const a = require('a')
-      const b = require('b')
-      const c = require('c').c.d
-      var e
-      e = c.e
-      const f = b.f
-      function main () {
-        c.foo()
-        e()
-      }
-    `, new Set(['a', 'c'])), dedent`
-      let a;
-
-      function get_a() {
-        return a = a || require('a');
-      }
-
-      const b = require('b')
-      let c;
-
-      function get_c() {
-        return c = c || require('c').c.d;
-      }
-
-      var e
-      function get_e() {
-        return e = e || get_c().e;
-      };
-      const f = b.f
-      function main () {
-        get_c().foo()
-        get_e()()
-      }
-    `)
-  })
-
-  test('top-level usage of deferred modules', () => {
-    assert.throws(() => {
-      deferRequires(dedent`
-        var a = require('a')
-        a()
-      `, new Set(['a']))
-    })
-    assert.throws(() => {
-      deferRequires(dedent`
-        require('a')()
-      `, new Set(['a']))
-    })
-  })
-
-  test('requires that appear in a closure wrapper defined in the top-level scope (e.g. CoffeeScript)', () => {
-    assert.equal(deferRequires(dedent`
-      (function () {
-        const a = require('a')
-        const b = require('b')
-        function main () {
-          return a + b
-        }
-      }).call(this)
-    `, new Set(['a'])), dedent`
-      (function () {
+    `
+    assert.equal(
+      processRequires({source, didFindRequire: (mod) => mod === 'a'}),
+      dedent`
         let a;
 
         function get_a() {
@@ -99,12 +26,91 @@ suite('deferRequires(source, deferredModules)', () => {
         function main () {
           return get_a() + b
         }
-      }).call(this)
+      `
+    )
+  })
+
+  test('top-level variables assignments that depend on previous requires', () => {
+    const source = dedent`
+      const a = require('a')
+      const b = require('b')
+      const c = require('c').c.d
+      var e
+      e = c.e
+      const f = b.f
+      function main () {
+        c.foo()
+        e()
+      }
+    `
+    assert.equal(
+      processRequires({source, didFindRequire: (mod) => ['a', 'c'].indexOf(mod) >= 0}),
+      dedent`
+        let a;
+
+        function get_a() {
+          return a = a || require('a');
+        }
+
+        const b = require('b')
+        let c;
+
+        function get_c() {
+          return c = c || require('c').c.d;
+        }
+
+        var e
+        function get_e() {
+          return e = e || get_c().e;
+        };
+        const f = b.f
+        function main () {
+          get_c().foo()
+          get_e()()
+        }
     `)
   })
 
+  test('top-level usage of deferred modules', () => {
+    assert.throws(() => {
+      processRequires({source: `var a = require('a'); a()`, didFindRequire: (mod) => true})
+    })
+    assert.throws(() => {
+      processRequires({source: `require('a')()`, didFindRequire: (mod) => true})
+    })
+  })
+
+  test('requires that appear in a closure wrapper defined in the top-level scope (e.g. CoffeeScript)', () => {
+    const source = dedent`
+      (function () {
+        const a = require('a')
+        const b = require('b')
+        function main () {
+          return a + b
+        }
+      }).call(this)
+    `
+    assert.equal(
+      processRequires({source, didFindRequire: (mod) => mod === 'a'}),
+      dedent`
+        (function () {
+          let a;
+
+          function get_a() {
+            return a = a || require('a');
+          }
+
+          const b = require('b')
+          function main () {
+            return get_a() + b
+          }
+        }).call(this)
+      `
+    )
+  })
+
   test('references to shadowed variables', () => {
-    assert.equal(deferRequires(dedent`
+    const source = dedent`
       const a = require('a')
       function outer () {
         console.log(a)
@@ -121,28 +127,32 @@ suite('deferRequires(source, deferredModules)', () => {
           console.log(a)
         }
       }
-    `, new Set(['a'])), dedent`
-      let a;
+    `
+    assert.equal(
+      processRequires({source, didFindRequire: (mod) => mod === 'a'}),
+      dedent`
+        let a;
 
-      function get_a() {
-        return a = a || require('a');
-      }
-
-      function outer () {
-        console.log(a)
-        function inner () {
-          console.log(a)
+        function get_a() {
+          return a = a || require('a');
         }
-        let a = []
-      }
 
-      function other () {
-        console.log(get_a())
-        function inner () {
+        function outer () {
+          console.log(a)
+          function inner () {
+            console.log(a)
+          }
           let a = []
-          console.log(a)
         }
-      }
-    `)
+
+        function other () {
+          console.log(get_a())
+          function inner () {
+            let a = []
+            console.log(a)
+          }
+        }
+      `
+    )
   })
 })
