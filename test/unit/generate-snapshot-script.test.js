6fe05ac1e7c62fedd3fb492b6f07490827b51ad5
Antonio Scandurra <as-cii@github.com>
2016-11-10 08:24:14 +0100

    Use vm.runInNewContext instead of eval

--- test/unit/generate-snapshot-script.test.js
+++ test/unit/generate-snapshot-script.test.js
@@ -1,13 +1,17 @@
+'use strict'
+
 const assert = require('assert')
 const path = require('path')
 const generateSnapshotScript = require('../../lib/generate-snapshot-script')
+const vm = require('vm')
 
 suite('generateSnapshotScript({baseDirPath, mainPath})', () => {
   test('simple integration test', () => {
     const baseDirPath = path.resolve(__dirname, '../fixtures/module')
     const mainPath = path.join(baseDirPath, 'index.js')
     const snapshotScript = generateSnapshotScript({baseDirPath, mainPath})
-    eval(snapshotScript)
-    assert.equal(snapshotResult.foo, 'abbA')
+    const sandbox = {}
+    vm.runInNewContext(snapshotScript, sandbox)
+    assert.equal(sandbox.snapshotResult.foo, 'abbA')
   })
 })
